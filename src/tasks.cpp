98db0531c901161d75e8454ca61e12c37b6484b9
jonas-ar <jonasfon@alu.ufc.br>
2025-07-07 17:24:46 -0300

    feat: implemetado a mediana para leitura mais precisa do sensor ultrassonico

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -1,5 +1,10 @@
 #include "tasks.hpp"
 
+// dados referente ao Wi-Fi
+const char* ssid = "NOME_REDE_WIFI";
+const char* senha = "SENHA_REDE_WIFI";
+const char* url = "http://exemplo.servidor/dados";
+
 // Definição das filas
 QueueHandle_t fila_umidade_solo;
 QueueHandle_t fila_chuva;
@@ -12,11 +17,6 @@ TaskHandle_t handleChuvaTask;
 TaskHandle_t handleIrrigacaoTask;
 TaskHandle_t handleNivelAgua;
 TaskHandle_t handleComunicacao;
-
-// dados referente ao Wi-Fi
-const char* ssid = "NOME_REDE_WIFI";
-const char* senha = "SENHA_REDE_WIFI";
-const char* url = "http://exemplo.servidor/dados";
 
 // Task para realizar a leitura do sensor de umidade do solo
 void vTaskSolo(void *pvParameters) {
@@ -40,23 +40,35 @@ void vTaskChuva(void *pvParameters) {
 
 // Task para realizar a leitura do sensor ultrassônico
 void vTaskNivelAgua(void *pvParameters) {
+  const int numLeituras = 5;
   long duracao;
-  float distancia_cm;
+  float leituras[numLeituras];
 
   while(1) {
-    digitalWrite(PIN_TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(PIN_TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(PIN_TRIG, LOW);
+    for (int i = 0; i < numLeituras; i++) {
+      digitalWrite(PIN_TRIG, LOW);
+      delayMicroseconds(2);
+      digitalWrite(PIN_TRIG, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(PIN_TRIG, LOW);
 
-    // medição do tempo de retorno do pulso
-    duracao = pulseIn(PIN_ECHO, HIGH);
+      duracao = pulseIn(PIN_ECHO, HIGH);
+      leituras[i] = duracao * 0.034 / 2;
 
-    // calcula a distância em cm
-    distancia_cm = duracao * 0.034 / 2;
+      vTaskDelay(pdMS_TO_TICKS(50)); // intervalo entre leituras
+    }
 
-    xQueueSend(fila_nivel_agua, &distancia_cm, portMAX_DELAY);
+    // ordena o vetor para obter a mediana
+    std::sort(leituras, leituras + numLeituras); // adapta o algoritmo de ordenação com base no tamanho do vetor
+
+    float mediana;
+    if (numLeituras % 2 == 0) {
+      mediana = (leituras[numLeituras / 2 - 1] + leituras[numLeituras / 2]) / 2.0;
+    } else {
+      mediana = leituras[numLeituras / 2];
+    }
+
+    xQueueSend(fila_nivel_agua, &mediana, portMAX_DELAY);
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
@@ -64,7 +76,7 @@ void vTaskNivelAgua(void *pvParameters) {
 // instância do motor
 AccelStepper stepper(AccelStepper::FULL4WIRE, PIN_IN1, PIN_IN3, PIN_IN2, PIN_IN4);
 
-// Task para realizar a irrigação. Implementar a lógica para acionar o motor após chuva intensa e muita umidade do solo
+// Task para realizar a irrigação
 void vTaskIrrigacao(void *pvParameters) {
   int solo_medicao;
   int estado_chuva;
@@ -81,21 +93,24 @@ void vTaskIrrigacao(void *pvParameters) {
         xQueueReceive(fila_chuva, &estado_chuva, portMAX_DELAY) &&
         xQueueReceive(fila_nivel_agua, &nivel_medicao, portMAX_DELAY)) {
 
-      Serial.printf("Valor bruto do sensor de umidade do solo: %d \n", solo_medicao);
-      Serial.printf("Valor bruto do sensor de chuva (0 chovendo, 1 sem chover): %d \n", estado_chuva);
-      Serial.printf("Distância em cm: %f\n\n", nivel_medicao);
+      // Serial.printf("Valor bruto do sensor de umidade do solo: %d \n", solo_medicao);
+      // Serial.printf("Valor bruto do sensor de chuva (0 chovendo, 1 sem chover): %d \n", estado_chuva);
+      // Serial.printf("Distância em cm: %f\n\n", nivel_medicao);
 
-      if(solo_medicao > 2500) {
-        Serial.println("RELE LIGADO, BAIXA HUMIDADE DO SOLO");
+      if (nivel_medicao >= 21.0) {
+        // Serial.println("Nível de água CRÍTICO, a bomba será desligada.");
+        digitalWrite(PIN_RELE, LOW);
+      } else if (solo_medicao > 2500) {
+        // Serial.println("Solo seco detectado. Bomba ligada para irrigação.");
         digitalWrite(PIN_RELE, HIGH);
       } else {
-        Serial.println("RELE DESLIGADO, UMIDADE ESTA OK!");
+        // Serial.println("Solo úmido. Bomba desligada.");
         digitalWrite(PIN_RELE, LOW);
-      }      
-
+      }
+    
       // se estiver chovendo e o estado atual da lona for aberta, será fechado
       if (estado_chuva == 0 && !lonaFechada) {
-        Serial.println("Está chovendo! acionando cobertura...");
+        // Serial.println("Está chovendo! acionando cobertura...");
         stepper.moveTo(stepper.currentPosition() + 2048); // fecha a cobertura
 
         while (stepper.distanceToGo() != 0) {
@@ -108,7 +123,7 @@ void vTaskIrrigacao(void *pvParameters) {
 
       // se a chuva parar e o estado atual da lona for fechado, será aberto
       if (estado_chuva == 1 && lonaFechada) {
-        Serial.println("Parou de chover! abrindo cobertura...");
+        // Serial.println("Parou de chover! abrindo cobertura...");
         stepper.moveTo(stepper.currentPosition() - 2048); // abre a cobertura
 
         while (stepper.distanceToGo() != 0) {
@@ -172,6 +187,6 @@ void vTaskComunicacao(void *pvParameters) {
       http.end();
     }
 
-    vTaskDelay(pdMS_TO_TICKS(2000));
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
