cdd6e95331f4ea459679670550ab0e18a5f682cc
jonas-ar <jonasfon@alu.ufc.br>
2025-07-09 20:08:52 -0300

    fix: otimizado uso de memória

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ void setup() {
   fila_nivel_agua = xQueueCreate(5, sizeof(float));
   fila_dados_irrigacao = xQueueCreate(5, sizeof(DadosIrrigacao));
   // cria as tasks
-  xTaskCreatePinnedToCore(vTaskSolo, "TASK_SOLO", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &handleSoloTask, 1);
-  xTaskCreatePinnedToCore(vTaskChuva, "TASK_CHUVA", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &handleChuvaTask, 1);
+  xTaskCreatePinnedToCore(vTaskSolo, "TASK_SOLO", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &handleSoloTask, 1);
+  xTaskCreatePinnedToCore(vTaskChuva, "TASK_CHUVA", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &handleChuvaTask, 1);
   xTaskCreatePinnedToCore(vTaskIrrigacao, "TASK_IRRIGACAO", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &handleIrrigacaoTask, 0);
   xTaskCreatePinnedToCore(vTaskNivelAgua, "TASK_NIVEL_AGUA", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &handleNivelAgua, 1);
   xTaskCreatePinnedToCore(vTaskComunicacao, "TASK_WIFI", configMINIMAL_STACK_SIZE + 2028, NULL, 1, &handleComunicacao, 0);
