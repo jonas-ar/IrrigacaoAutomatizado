f450b97097a6597eca555abd99b9fff63e473dd9
jonas-ar <jonasfon@alu.ufc.br>
2025-07-16 10:54:27 -0300

    fix: corrigido o stach overflow

--- src/main.cpp
+++ src/main.cpp
@@ -36,11 +36,11 @@ void setup() {
   xTaskCreatePinnedToCore(vTaskSolo, "TASK_SOLO", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &handleSoloTask, 1);
   xTaskCreatePinnedToCore(vTaskMotor, "TASK_MOTOR", configMINIMAL_STACK_SIZE + 1024, NULL, 1, NULL, 0);
   xTaskCreatePinnedToCore(vTaskChuva, "TASK_CHUVA", configMINIMAL_STACK_SIZE + 1024, NULL, 1, &handleChuvaTask, 1);
-  xTaskCreatePinnedToCore(vTaskIrrigacao, "TASK_IRRIGACAO", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &handleIrrigacaoTask, 0);
+  xTaskCreatePinnedToCore(vTaskIrrigacao, "TASK_IRRIGACAO", configMINIMAL_STACK_SIZE + 4096, NULL, 1, &handleIrrigacaoTask, 0);
   xTaskCreatePinnedToCore(vTaskNivelAgua, "TASK_NIVEL_AGUA", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &handleNivelAgua, 1);
-  xTaskCreatePinnedToCore(vTaskComunicacao, "TASK_WIFI", configMINIMAL_STACK_SIZE + 2028, NULL, 1, &handleComunicacao, 0);
+  xTaskCreatePinnedToCore(vTaskComunicacao, "TASK_WIFI", configMINIMAL_STACK_SIZE + 8192, NULL, 1, &handleComunicacao, 0);
   // configura o watchdog
-  esp_task_wdt_init(5, true); // inicia o watchdog com 5 segundos de timeout
+  esp_task_wdt_init(10, true); // inicia o watchdog com 10 segundos de timeout
   esp_task_wdt_add(handleIrrigacaoTask);
   esp_task_wdt_add(handleComunicacao);
 }
